482d5a8a9a3cc8daafa27eb885c0108306059596
nsheludkov <nsheludkov@ya.ru>
2023-01-12 21:03:40 +0300

    init: add revesre function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    nstr = Math.abs(n).toString();
+    let newnstr ="";
+    digitCount = nstr.length-1;
+    for (var i=digitCount ; i >= 0 ; i--) {
+        newnstr = newnstr + nstr[i];
+    }
+    return Number(newnstr);
 }
